b93cb3a007438235b5ff10fc5472629797805855
Limchangsu <skyisthelimitless@gmail.com>
2020-07-04 12:40:55 +0900

    Working with Providers & Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping_app/providers/products.dart';
 import 'package:shopping_app/screens/product_detail_screen.dart';
 import 'package:shopping_app/screens/products_overview_screen.dart';
 
@@ -10,17 +12,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
---
+++ lib/providers/products.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/models/product.dart';
+
+class Products with ChangeNotifier {
+  List<Product> _items = [
+    Product(
+        id: 'p1',
+        title: 'Red Shirt',
+        description: 'A red shirt - it is pretty red!',
+        price: 29.99,
+        imageUrl:
+        'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_960_720.jpg'),
+    Product(
+        id: 'p2',
+        title: 'Blue Shirt',
+        description: 'A blue shirt - it is pretty blue!',
+        price: 19.99,
+        imageUrl:
+        'https://cdn.pixabay.com/photo/2016/01/19/17/53/writing-1149962_960_720.jpg'),
+  ];
+
+  List<Product> get items {
+    return [..._items];
+  }
+
+  void addProduct() {
+//    _items.add(value);
+    notifyListeners();
+  }
+}
