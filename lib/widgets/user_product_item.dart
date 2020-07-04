3f5672940bb3f40125ed3b166aa37e6750742827
Limchangsu <skyisthelimitless@gmail.com>
2020-07-04 22:42:33 +0900

    Time to Update Products

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/screens/edit_product_screen.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  const UserProductItem({Key key, this.title, this.imageUrl}) : super(key: key);
+  const UserProductItem({Key key, this.id, this.title, this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +21,9 @@ class UserProductItem extends StatelessWidget {
           children: <Widget>[
             IconButton(
               icon: Icon(Icons.edit),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(EditProductScreen.routeName, arguments: id);
+              },
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
