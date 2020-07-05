f7f48fcac8d6ca0df1c7907577c981dfd01694bc
Limchangsu <skyisthelimitless@gmail.com>
2020-07-05 15:40:30 +0900

    Using the FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -16,31 +16,49 @@ class _OrdersScreenState extends State<OrdersScreen> {
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+//    Future.delayed(Duration.zero).then((_) async {
+
+//      _isLoading = true;
+//      Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+//        setState(() {
+//          _isLoading = false;
+//        });
+//      });
+
+//    });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading ? Center(
-        child: CircularProgressIndicator(),
-      ) : ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              // Do error handling stuff
+              return Container();
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) {
+                  return ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                  );
+                },
+              );
+            }
+          }
+        },
       ),
     );
   }
